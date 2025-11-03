2a6f33e3eed8f131104c7326902b4b6e275f8242
mzxxzysy <jsyjj21@gmail.com>
2025-11-03 17:39:28 +0900

    ✨ Feat: 알림 페이지 퍼블리싱 (#3)

--- FindMEmory/FindMEmory/Notification/NotificationView.swift
+++ FindMEmory/FindMEmory/Notification/NotificationView.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct NotificationView: View {
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
-        Text("NotificationView")
+        HeaderGroup
+        NotificationList
+        Spacer()
+    }
+    
+    private var HeaderGroup: some View {
+        ZStack{
+            HStack{
+                Button(action: { dismiss() }, label: {
+                    Image(systemName: "chevron.backward")
+                        .tint(.black)
+                })
+                Spacer()
+            }
+            .padding()
+            Text("알림")
+        }
+    }
+    
+    private var NotificationList: some View {
+        VStack{
+            NotificationItem(notification: Notification(type: "adopted", title: "이 캐릭터 나온 만화", date: "1분 전"))
+        }
     }
 }
 
